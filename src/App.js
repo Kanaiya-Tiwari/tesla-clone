6f2959bab651e02cc4e4d3bf31a9a01aebc1795a
kannaiya <kannaiyatiwari892@gmail.com>
2022-04-08 19:31:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import React, { Fragment, useEffect, useState } from 'react'
 import './App.css';
+import Header from './Header';
+import { BrowserRouter as Router, Routes,Route, Redirect, Navigate } from 'react-router-dom';
+import Menu from './Menu'
+import HeaderBlock from './HeaderBlock'
+import Login from './Login'
+import { useDispatch, useSelector } from 'react-redux'
+import { login } from './features/userSlice'
+import { logout, selectUser } from './features/userSlice'
+import Signup from './Signup'
+import TeslaAccount from './TeslaAccount'
+import { auth } from './firebase'
+import { onAuthStateChanged } from 'firebase/auth';
 
 function App() {
+  const user = useSelector(selectUser)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    onAuthStateChanged(auth,(userAuth) => {
+      if (userAuth) {
+        // User is signed in
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+          })
+        )
+      } else {
+        // User is signed out
+        dispatch(logout())
+      }
+    })
+  }, [dispatch])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="App">
+      <Routes>
+     <Route  path="/" exact  element={<Fragment><Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} /><HeaderBlock/>{isMenuOpen && <Menu/>}</Fragment>}/>
+     <Route  path="/login"  element={<>{user? <Navigate  to="/teslaaccount"/>:<Login/>} </>}/>
+     <Route  path="/signup"  element={<Signup/>}/>
+     <Route  path="/teslaaccount"  element={<>{!user? <Navigate  to="/login"/>: <TeslaAccount isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />}{isMenuOpen && <Menu/>}</>}/>
+     </Routes>
     </div>
+    </Router>
+  
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth} from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDw97i-tZvm98yekdoTUiEBvqiJcCfu0e8",
+  authDomain: "tesla-clone-2a305.firebaseapp.com",
+  projectId: "tesla-clone-2a305",
+  storageBucket: "tesla-clone-2a305.appspot.com",
+  messagingSenderId: "184654295030",
+  appId: "1:184654295030:web:299cf9fd3961d5fc6e21d7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth=getAuth(app);
+export {auth};
